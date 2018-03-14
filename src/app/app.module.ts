ffb22bdc09a03724a3b72d7102f4546ec1dadb5a
Martin Tracey <matracey@outlook.ie>
2018-03-14 17:09:29 +0000

    Added card.component to the app.component. Added bootstrap and font-awesome. Added vscode configuration.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
-
 import { AppComponent } from "./Components/app//app.component";
+import { CardComponent } from "./Components/card/card.component";
 
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        CardComponent,
     ],
     imports: [
         BrowserModule
